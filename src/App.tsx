b314bedd07f388864ed2335f89516d2c2f8030f5
Gideon Aryeetey <gideonaryeetey8@gmail.com>
2022-09-09 13:04:48 +0000

    chore: handle complaint response and render

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React, { useQuery } from '@apollo/client';
-import { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
 
+import ComplaintTable from './components/Complaint/ComplaintTable';
 import { FETCH_COMPLAINTS } from './graphql/queries';
+import { IComplaint } from './interfaces/complaint';
 
 function App() {
   const { loading, data } = useQuery(FETCH_COMPLAINTS);
+  const [complaints, setComplaints] = useState<IComplaint[]>([]);
 
   useEffect(() => {
     if (!loading) {
-      console.log(data);
+      const complaint: IComplaint[] = data?.complaint;
+      if (Array.isArray(complaint)) {
+        setComplaints(complaint);
+      }
     }
   }, [data, loading]);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="max-w-5xl mx-auto p-4">
+        <button>Create new complaint</button>
+        <ComplaintTable complaints={complaints} />
+      </div>
+    </>
+  );
 }
 
 export default App;
